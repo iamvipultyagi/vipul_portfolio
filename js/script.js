9fd7086f8c2c585e9c9631e0b377ca9e471b4ca6
iamvipultyagi <88539287+iamvipultyagi@users.noreply.github.com>
2025-11-07 09:53:44 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const menu= document.querySelector("#menu-icon");
+const navlist= document.querySelector(".navlist");
+
+menu.addEventListener("click",()=>{
+    menu.classList.toggle("open");
+    navlist.classList.toggle("open");
+})
+document.querySelectorAll(".navlist").forEach(n => n.addEventListener("click", ()=>{
+    menu.classList.remove("open");
+    navlist.classList.remove("open");
+}))
+// ACTIVE LINK
+const section =document.querySelectorAll("section");
+const links = document.querySelectorAll("a");
+
+window.onscroll = ()=>{
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            links.forEach(link =>{
+                link.classList.remove("active");
+                document.querySelector("header ul li a[href*="+ id +"]").classList.add("active");
+            })
+        }
+    })
+}
+
+
+// TYPED SCRIPT HOME
+
+// TYPED SCRIPT HOME
+
+var typed =  new Typed(".auto-type",{
+    strings: ["Full Stack Web Developer","Frontend Developer","Full Stack Web Developer","Backend Developer"],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true
+})
+// TYPED SCRIPT FOOTER
+
+var typed =  new Typed(".hero",{
+    strings: ["Vipul Tyagi &#x2764;"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
